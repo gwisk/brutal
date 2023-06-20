e1f2f259b44baf6484016f3837bc53833468cb0a
George Iskander <georgeiskander@uchicago.edu>
2023-06-20 17:24:15 -0500

    init commit
    init commit, still lotta stuff to work on

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,15 +3,16 @@ import { z, defineCollection } from "astro:content";
 const blogCollection = defineCollection({
 	schema: z.object({
 		title: z.string(),
+		subtitle z.string(),
 		author: z.string(),
 		tags: z.array(z.string()),
 		description: z.string(),
-		pubDate: z.string().transform((str) => new Date(str)),
+		pubDate: z.string(),
 		imgUrl: z.string(),
-		draft: z.boolean().optional().default(false),
+		draft: z.boolean().optional().default(false)
 	}),
 });
 
 export const collections = {
-	blog: blogCollection,
+	'blog': blogCollection
 };
