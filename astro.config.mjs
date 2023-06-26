9fdb821bff3ebfd7ee364b39c587faa7a1f9bee0
George Iskander <georgeiskander@uchicago.edu>
2023-06-26 17:42:51 -0500

    more changes
    - compression?
    - started to add analytics
    - socials re-arranged

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,24 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
 import { remarkReadingTime } from './remark-reading-time.mjs';
+import partytown from "@astrojs/partytown";
+import compress from "astro-compress";
 
+import compressor from "astro-compressor";
+
+// https://astro.build/config
 export default defineConfig({
   // used to generate images
-  site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://brutal.elian.codes/'
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3001/',
+  site: process.env.VERCEL_ENV === 'production' ? 'https://brutal.elian.codes/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'https://localhost:3001/',
   trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({ injectReset: true })],
-  markdown : {
-    remarkPlugins: [remarkReadingTime],
+  integrations: [sitemap(), UnoCSS({
+    injectReset: true
+  }), partytown({
+    config: {
+      forward: ["datalayer.push"]
+    }
+  }), compress()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime]
   }
 });
