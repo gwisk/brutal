f3f8551a2f2089d06d2c8f8624a7418df8503fd8
George Iskander <georgeiskander@uchicago.edu>
2023-08-20 17:00:03 -0500

    fixed borders + big package updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import UnoCSS from 'unocss/astro';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 import compress from "astro-compress";
 import sitemap from '@astrojs/sitemap';
-import { astroImageTools } from "astro-imagetools";
+// import { astroImageTools } from "astro-imagetools";
 import vue from "@astrojs/vue";
 import robotsTxt from "astro-robots-txt";
 
@@ -12,9 +12,14 @@ export default defineConfig({
   // used to generate images
   site: process.env.VERCEL_ENV === 'production' ? 'https://www.filmslop.com/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'https://localhost:3001/',
   trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({
-    injectReset: true
-  }), vue(), astroImageTools, robotsTxt(), compress()],
+  integrations: [
+    sitemap(), 
+    UnoCSS({
+      injectReset: true
+    }), 
+    vue(), 
+    robotsTxt(),
+    compress()],
   markdown: {
     remarkPlugins: [remarkReadingTime]
   },
@@ -22,5 +27,8 @@ export default defineConfig({
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"]
     }
-  }
+  },
+  experimental: {
+    assets: true
+   }
 });
