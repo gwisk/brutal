9db56dba8531e9f03a90c00969b5432853198673
George Iskander <georgeiskander@uchicago.edu>
2023-11-21 22:58:44 -0600

    ahmed

--- scripts/replaceAssetPaths.js
+++ scripts/replaceAssetPaths.js
@@ -1,18 +1,21 @@
-// /scripts/replaceAssetPaths.js
+// scripts/replaceAssetPaths.js
 
-// all .md files in this directory and subdirectories will be processed
+// all .md files in this directory will be processed
+// specify from the root of your project
 const postsDirectory = "/src/content/";
 
 // Find and replace strings
 const find = "/src/assets/";
-const replace = "~/assets/";
+let replace = "../src/assets/"; // update this path based on where files are located
+
+replace = replace.replaceAll(".", "\\.");
 // Special characters (https://en.wikipedia.org/wiki/Regular_expression#POSIX_basic_and_extended) need to be escaped
 
 import { exec } from "child_process";
 // execute bash command
 exec(
   `find ${process.cwd()}${postsDirectory} -type f -name '*.md' -print0 | xargs -0 sed -i -e 's:${find}:${replace}:g'`,
-  // Note: GNU sed that runs on Linux but not on mac (most servers use Linux)
+  // GNU sed that runs on Linux but not on mac
   (error, stdout, stderr) => {
     // error handling
     if (error) {
